02a86f5097132ec4588f96ba02baf660989cbc39
Šimon Pavlík <SimonPavlik@users.noreply.github.com>
2018-06-01 09:36:00 +0200

    folder2csv: extracted texts can now be in unicode

--- folder2csv.py
+++ folder2csv.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
 
         fstr = defaultdict(list)
         for f in r.get('fields', []):
-            if isinstance(f['content'], str):
+            if isinstance(f['content'], unicode) or isinstance(f['content'], str):
                 fstr[f['title']].append(f['content'])
             else:
                 fstr[f['title']].append('\n'.join('%s: %s' % (sf['title'], sf['content']) for sf in f['content']))
